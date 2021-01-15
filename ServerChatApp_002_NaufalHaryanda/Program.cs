fb3c5adf0623f56bfca3dd3fd9fa060e205c2552
NaufalHaryanda <naufalharyanda10@gmail.com>
2021-01-15 14:04:45 +0700

    Add project files.

---
+++ ServerChatApp_002_NaufalHaryanda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_002;
+
+namespace ServerChatApp_002_NaufalHaryanda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObject = null;
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Server ON, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
